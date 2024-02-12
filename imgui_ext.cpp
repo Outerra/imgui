f6ce272b6220aaef9106478116f1f18a0f242032
brano.kemen <cameni@gmail.com>
2024-02-12 13:10:51 +0100

    fixed TextFilter width handling

--- imgui_ext.cpp
+++ imgui_ext.cpp
@@ -119,12 +119,11 @@ void TextClipped(ImStrv text, float max_width, ImGuiExTextClippedFlags flags)
     ImGui::SetCursorScreenPos(textRect.Max - ImVec2{ 0, textSize.y + window->DC.CurrLineTextBaseOffset });
 }
 
-bool TextFilter(const char* hint, char* buf, size_t buf_size, float width)
+bool TextFilter(ImStrv hint, char* buf, size_t buf_size)
 {
     ImGui::PushID(hint);
-    ImGui::PushItemWidth(width);
+    ImGui::BeginGroup();
     bool result = ImGui::InputTextWithHint("##filter", hint, buf, buf_size);
-    ImGui::PopItemWidth();
     if (*buf != '\0') {
         ImGui::SetItemAllowOverlap();
         ImGui::SameLine(0, -1);
@@ -134,6 +133,7 @@ bool TextFilter(const char* hint, char* buf, size_t buf_size, float width)
             result = true;
         }
     }
+    ImGui::EndGroup();
     ImGui::PopID();
     return result;
 }
