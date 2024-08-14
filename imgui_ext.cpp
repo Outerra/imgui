fc5737ef82c1bc25fea5dfba16c2e9b84300de00
brano.kemen <cameni@gmail.com>
2024-08-14 17:51:33 +0200

    SliderIntCheckbox

--- imgui_ext.cpp
+++ imgui_ext.cpp
@@ -616,6 +616,27 @@ bool SliderFloat(const char* label, float* v, float v_min, float v_max, const ch
     return result;
 }
 
+bool SliderIntCheckbox(ImStrv label, bool* s, int* v, int v_min, int v_max, const char* format, ImGuiSliderFlags flags)
+{
+    //shows checkbox before the value, for combined and compact enable+value display
+    bool state = ImGuiEx::LabelCheck(label, s);
+    bool disabled = *s == false;
+    if (disabled) {
+        format = "";
+        ImGui::BeginDisabled();
+    }
+
+    ImGui::PushID(label);
+    bool result = ImGui::SliderInt("##SliderIntCheckbox", v, v_min, v_max, format, flags);
+    if (ImGui::IsItemDeactivatedAfterEdit())
+        *s = true;
+    ImGui::PopID();
+
+    if (disabled)
+        ImGui::EndDisabled();
+    return state || result;
+}
+
 bool SliderFloatCheckbox(ImStrv label, bool* s, float* v, float v_min, float v_max, const char* format, ImGuiSliderFlags flags)
 {
     //shows checkbox before the value, for combined and compact enable+value display
@@ -627,7 +648,7 @@ bool SliderFloatCheckbox(ImStrv label, bool* s, float* v, float v_min, float v_m
     }
 
     ImGui::PushID(label);
-    bool result = ImGui::SliderFloat("##SliderFloatCheckbox2", v, v_min, v_max, format, flags);
+    bool result = ImGui::SliderFloat("##SliderFloatCheckbox", v, v_min, v_max, format, flags);
     if (ImGui::IsItemDeactivatedAfterEdit())
         *s = true;
     ImGui::PopID();
