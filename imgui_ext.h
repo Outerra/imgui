3b6d8c00f6c85ad7d6eb04320eca70a10d4ac0c4
brano.kemen <cameni@gmail.com>
2025-04-28 16:07:48 +0200

    InputBitfield support for aliases ("F|Force" ...), tooltips

--- imgui_ext.h
+++ imgui_ext.h
@@ -359,7 +359,7 @@ IMGUI_API void EndListBox();
 
 IMGUI_API bool MultistateToggleButton(ImStrv label, int* current_item, const char* items_separated_by_zeros);
 IMGUI_API bool MultistateToggleButton(ImStrv label, int* current_item, const char** items_terminated_by_zero, const char** tooltips = 0, uint inactive_mask = 0, bool reverse = false);
-IMGUI_API bool InputBitfield(ImStrv label, uint* bits, const char* items_separated_by_zeros, ImGuiExInputBitfieldFlags flags = 0);
+IMGUI_API bool InputBitfield(ImStrv label, uint* bits, const char** items_terminated_by_zero, ImGuiExInputBitfieldFlags flags = 0);
 
 IMGUI_API bool ActiveButton(const char* label, bool active, const ImVec2& size_arg = ImVec2(0, 0));
 
